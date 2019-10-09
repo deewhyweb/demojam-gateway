74b778b0130c57db9e06050c74cec3602009553a
Philip Hayes <phayes@redhat.com>
2019-10-09 10:19:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const state = { isReady: false };
+const PORT = process.env["PORT"] ? process.env("PORT") : 8080;
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(PORT);
+
+app.use('/', require("./lib/routes"));
+
+
+process.on('SIGTERM', function onSigterm () {
+    console.info('Got SIGTERM. Graceful shutdown start now', new Date().toISOString())
+    state.isReady = false;
+
+});
+
+io.on('connection', function (socket) {
+    var entries = 0;
+    console.log('connection')
+    setInterval(() => {
+        const max = 15;
+        const min = 0;
+        const selector = Math.floor(Math.random() * (+max - +min)) + +min;
+        if (selector <= 2){
+            entries++;
+        }
+        
+        socket.emit('message', {entries});
+    }, 100)
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+});
+
